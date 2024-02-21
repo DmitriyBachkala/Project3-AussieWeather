9397343e6c76757d31b977b6739e85bad2f22e7f
DmitriyBachkala <141796351+DmitriyBachkala@users.noreply.github.com>
2024-02-20 20:41:21 -0700

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -5,12 +5,12 @@ let stateSummary = "/Data/state_summary.json";
 
 let weatherType = "Rain"; // Initialize weatherType with "Rain"
 
-// Fetch the JSON data (with d3.csv) and console log it
-d3.csv(stateSummary).then(function(result) {
-    data = result;
-    console.log(data);
-    updateDropdown(); // Call the function after data is fetched
-});
+// // Fetch the JSON data (with d3.csv) and console log it
+// d3.csv(stateSummary).then(function(result) {
+//     data = result;
+//     console.log(data);
+//     updateDropdown(); // Call the function after data is fetched
+// });
  
 // Create functions
 function updateDropdown() {
